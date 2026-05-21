7c6bfcb284096a976e2f5dfe26cc0c6ec7954aa4
rahulthapa9024 <rahulthapa9024@gmail.com>
2026-05-21 15:26:58 +0530

    adding Server URLs in backend and frontend

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(morgan("dev"))
 
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:process.env.FRONTEND_URL,
     credentials:true
 }));
 app.use(express.json());
